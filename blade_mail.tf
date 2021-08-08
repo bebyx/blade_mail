ad2f28b876f4bb6f91d4971be9271ba7d9e70ad6
bebyx <bebyx@protonmail.com>
2021-08-09 01:35:36 +0300

    Copy artifact to remote host

--- blade_mail.tf
+++ blade_mail.tf
@@ -15,6 +15,18 @@ resource "aws_instance" "blade-mail" {
 }
 
 resource "null_resource" "ansible" {
+
+  provisioner "file" {
+    source      = "artifacts/gopher.tar.gz"
+    destination = "/tmp/gopher.tar.gz"
+    connection {
+      type        = "ssh"
+      user        = "admin"
+      private_key = tls_private_key.blade-mail-ssh.private_key_pem
+      host        = aws_eip.ip-blade-mail.public_ip
+    }
+  }
+
   provisioner "remote-exec" {
     inline = ["echo 'Definitely connected!'"]
     connection {
